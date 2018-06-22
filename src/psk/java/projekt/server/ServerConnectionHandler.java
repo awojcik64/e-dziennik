d87ccd71cac55b2934e5b0e0ccb822be97ac220b
Aleksander <aleks4@outlook.com>
2018-06-22 19:14:06 +0200

    Kolejne drobne zmiany - w serwerze.

--- src/psk/java/projekt/server/ServerConnectionHandler.java
+++ src/psk/java/projekt/server/ServerConnectionHandler.java
@@ -2,15 +2,24 @@ package psk.java.projekt.server;
 
 import java.net.Socket;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ServerConnectionHandler implements Runnable {
     private Socket client;
-
+    private Connection db;
 
     public ServerConnectionHandler(Socket client) {
         this.client = client;
         Thread thread = new Thread(this, "e-dziennik Connection Handler");
+        try {
+            db=DriverManager.getConnection("jdbc:oracle:thin:@alekstablefield.asuscomm.com:27020:XE","edziennik","B@zka123!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 
     @Override
     public void run() {
